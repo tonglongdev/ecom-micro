9d3bc7159c14af7569a5cf8e5feddf68b6926a5f
Tong Long Dev <tonglong.dev@gmail.com>
2025-12-27 14:41:39 +0700

    fetch single product

--- apps/client/src/app/products/[id]/page.tsx
+++ apps/client/src/app/products/[id]/page.tsx
@@ -2,34 +2,22 @@ import ProductInteraction from "@/components/ProductInteraction";
 import { ProductType } from "@repo/types";
 import Image from "next/image";
 
-// TEMPORARY
-const product: ProductType = {
-  id: 1,
-  name: "Adidas CoreFit T-Shirt",
-  shortDescription:
-    "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-  description:
-    "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-  price: 59.9,
-  sizes: ["xs", "s", "m", "l", "xl"],
-  colors: ["gray", "purple", "green"],
-  images: {
-    gray: "/products/1g.png",
-    purple: "/products/1p.png",
-    green: "/products/1gr.png",
-  },
-  categorySlug: "test",
-  createdAt: new Date(),
-  updatedAt: new Date(),
+const fetchProduct = async (id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_PRODUCT_SERVICE_URL}/products/${id}`
+  );
+  const data: ProductType = await res.json();
+  return data;
 };
 
 export const generateMetadata = async ({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) => {
-  // TODO:get the product from db
-  // TEMPORARY
+  const { id } = await params;
+
+  const product = await fetchProduct(id);
   return {
     title: product.name,
     describe: product.description,
@@ -44,6 +32,9 @@ const ProductPage = async ({
   searchParams: Promise<{ color: string; size: string }>;
 }) => {
   const { size, color } = await searchParams;
+  const { id } = await params;
+
+  const product = await fetchProduct(id);
 
   const selectedSize = size || (product.sizes[0] as string);
   const selectedColor = color || (product.colors[0] as string);
